ef0453152859219f5b55936e244df6d46d7a209e
atmay <liubov.burtseva@gmail.com>
2020-10-04 18:54:19 +0300

    Code cleaned up; Notes added; Data migrated;

--- Bot.py
+++ Bot.py
@@ -11,7 +11,9 @@ Config.Load()
 
 TELEGRAM_TOKEN = Config.data['TELEGRAM']['TOKEN']
 TELEGRAM_USERS = Config.data['TELEGRAM']['SUBSCRIBERS']
-#CHAT_ID = Config.data['TELEGRAM']['SUBSCRIBERS'][0]['ID']
+
+
+# CHAT_ID = Config.data['TELEGRAM']['SUBSCRIBERS'][0]['ID']
 
 
 def AddNewUser(id, status='USER'):
@@ -21,13 +23,9 @@ def AddNewUser(id, status='USER'):
     })
     Config.Save()
 
+
 # Бот ждёт входящее сообщение /Start
 # Бот берёт ID написавшего человека, проверяет есть ли он в списке и если нет - добавляет.
-
-
-def send_message(message):
-    bot = telegram.Bot(token=TELEGRAM_TOKEN)
-    return bot.send_message(chat_id=CHAT_ID, text=message)
 
 
 def start(update, context):
@@ -37,45 +35,36 @@ def start(update, context):
     if any(x['ID'] != id for x in TELEGRAM_USERS):
         AddNewUser(id)
 
-messages = None
 
-def LoadMessages():
-    global messages
-    r = requests.get("https://raw.githubusercontent.com/atmay/MyInspirationBot/master/Messages.txt")
-    lines = r.text.split("\n")
-    messages = [line.strip() for line in lines]
+def add(update, context):
+    text = update.message.text.replace('/add ', '').strip()
+
+    Config.data['MESSAGES'].append(text)
+    Config.Save()
+
+    id = update.effective_chat.id
+    context.bot.send_message(chat_id=id, text=f"Message '{text}' added!")
 
 
 def main():
-    global messages
-    LoadMessages()
     # current_timestamp = int(time.time())  # начальное значение timestamp
 
     updater = Updater(token=TELEGRAM_TOKEN, use_context=True)
     dispatcher = updater.dispatcher
     dispatcher.add_handler(CommandHandler('start', start))
+    dispatcher.add_handler(CommandHandler('add', add))
     updater.start_polling()
 
     bot = telegram.Bot(token=TELEGRAM_TOKEN)
 
     while True:
+        messages = Config.data['MESSAGES']
         for s in TELEGRAM_USERS:
             id = s["ID"]
-            message = messages[random.randint(0, len(messages))]
+            message = messages[random.randint(0, len(messages) - 1)]
             print(f'Sending message "{message}" to {id}')
             bot.send_message(chat_id=id, text=message)
         time.sleep(Config.data['GLOBAL']['MESSAGE_DELAY'])
-        # try:
-        #     new_homework = get_homework_statuses(current_timestamp)
-        #     if new_homework.get('homeworks'):
-        #         send_message(parse_homework_status(new_homework.get('homeworks')[0]))
-        #     current_timestamp = new_homework.get('current_date')  # обновить timestamp
-        #
-        #
-        # except Exception as e:
-        #     print(f'Бот упал с ошибкой: {e}')
-        #     time.sleep(5)
-        #     continue
 
 
 if __name__ == '__main__':
